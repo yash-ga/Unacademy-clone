4e289f07e82dfcabc86f16c6017cdbf9625616dc
kapil <kapilmogre1998@gmail.com>
2021-12-15 13:23:33 +0530

    landing page login and footer is remaining

--- unacademy2.0/src/App.js
+++ unacademy2.0/src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import Goal from './components/goal/Goal';
 import { LandingPage } from './components/Landing/LandingPage';
 
 function App() {
   return (
     <div>
-     <Goal/>
      <LandingPage/>
     </div>
   );
